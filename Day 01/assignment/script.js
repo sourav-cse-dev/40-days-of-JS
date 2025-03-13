53a6fb2538ffbd68b8f831301ff07b7b3dc395e8
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-03-13 15:27:41 +0600

    day-01 practice & assignment completed

---
+++ Day 01/assignment/script.js
@@ -0,0 +1,5 @@
+console.log("Hello, tapaScript!");
+console.log("Welcome to 40 Days of JavaScript!");
+document.write("Check the browser console for a message!");
+
+document.getElementById("dynamicContent").innerText = "Wish me luck!";
